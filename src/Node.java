7ec96c7c57e159cc2bb5fff15daea327898b0613
pham19 <pham19@SNO9.Tacoma.UW.EDU>
2019-04-03 13:18:44 -0700

    Theory

---
+++ src/Node.java
@@ -0,0 +1,10 @@
+
+public class Node {
+	private int data;
+	private Node pNext;
+	
+	public Node(int theData, Node thePNext) {
+		data = theData;
+		pNext = thePNext;
+	}
+}
---
+++ src/SinglyListNode.java
@@ -0,0 +1,32 @@
+public class SinglyListNode {
+	private int size;
+	private Node pHead;
+	
+	public void addFrist(Node theNode) {
+		
+	}
+	
+	public void add(Node theNode, int thePosition) {
+		
+	}
+	
+	public void delete(Node theNode, int thePosition) {
+		
+	}
+	
+	public void deleteLastNode() {
+		
+	}
+	
+	public void edit(Node theNode, int thePosition) {
+		
+	}
+	
+	public Node findData(int theData) {
+		Node node = null;
+		
+		return node;
+	}
+	
+	
+}
